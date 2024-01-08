bdc9904dfce2193b567d1a2cfbda73e9b541a486
Henrique Oliveira Gobi <102251338+HenriqueOgobi@users.noreply.github.com>
2024-01-08 14:58:55 -0300

    First commit

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#131316",
+      padding: 24,
+    },
+    eventName: {
+      color: "#FDFCFE",
+      fontSize: 24,
+      fontWeight: "bold",
+      marginTop: 48,
+    },
+    EventDate: {
+      color: "#6B6B6B",
+      fontSize: 16,
+    },
+  });
