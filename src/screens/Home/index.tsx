0fbaee76a0fd6fd0a8c082f6ec452e497198f433
Henrique Oliveira Gobi <102251338+HenriqueOgobi@users.noreply.github.com>
2024-01-08 18:10:26 -0300

    Second commit

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,26 @@
-import { Text, View } from "react-native";
-import {styles} from './styles'
+import { Text, View, TextInput, TouchableOpacity } from "react-native";
+import { styles } from "./styles";
 
 export function Home() {
+  function handleParticipantAdd() {
+    console.log("Voce clicou no botao de adicionar!");
+  }
   return (
     <View style={styles.container}>
-      
       <Text style={styles.eventName}>Nome do evento </Text>
 
       <Text style={styles.EventDate}>Sexta, 4 de Novembro de 2022.</Text>
+
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do Participante"
+          placeholderTextColor="#6B6B6B"
+        />
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
