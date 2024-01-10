777a5ae3789cf775de08f3388ddda3d4ebb87ab4
Henrique Oliveira Gobi <102251338+HenriqueOgobi@users.noreply.github.com>
2024-01-10 15:31:31 -0300

    Final commit
    Here i finish this app. now we can add peoples on this list and remove,  i traned all fundaments of the react-native.

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,41 @@
-import { Text, View, TextInput, TouchableOpacity } from "react-native";
+import {
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+  FlatList,
+  Alert,
+} from "react-native";
+import react, { useState } from "react";
 import { styles } from "./styles";
 
+import { Participant } from "../../components/Participant";
+
 export function Home() {
+  const [participants, setParticipants] = useState<string[]>([]);
+  const [participantName, setParticipantName] = useState("");
+
   function handleParticipantAdd() {
-    console.log("Voce clicou no botao de adicionar!");
+    if (participants.includes(participantName)) {
+      return Alert.alert(
+        "Participante Existe",
+        "Ja existe um participante na Lista com esse nome"
+      );
+    }
+    setParticipants((prevState) => [...prevState, participantName]);
+    setParticipantName("");
   }
+
+  function handleParticipantRemove(name: String) {
+    Alert.alert("Remover", `Remover o participante ${name}?`, [
+      { text: "Sim",    
+      onPress:() => setParticipants(prevState => prevState.filter(participant => participant !== name))
+    },
+      { text: "Nao", style: "Cancel" },
+    ]);
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do evento </Text>
@@ -16,11 +47,30 @@ export function Home() {
           style={styles.input}
           placeholder="Nome do Participante"
           placeholderTextColor="#6B6B6B"
+          onChangeText={setParticipantName}
+          value={participantName}
         />
         <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
           <Text style={styles.buttonText}>+</Text>
         </TouchableOpacity>
       </View>
+      <FlatList
+        data={participants}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Participant
+            key={item}
+            name={item}
+            onRemove={() => handleParticipantRemove(item)}
+          />
+        )}
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={() => (
+          <Text style={styles.listEmptyText}>
+           Ninguém chegou no evento ainda? Adicione participantes a sua lista de presença.
+          </Text>
+        )}
+      />
     </View>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -16,31 +16,37 @@ export const styles = StyleSheet.create({
     fontSize: 16,
   },
   input: {
-    flex:1,
+    flex: 1,
     height: 56,
     backgroundColor: "#1F1E25",
     borderRadius: 5,
     color: "#FFF",
     padding: 16,
     fontSize: 16,
-    marginRight:12
+    marginRight: 12,
   },
   buttonText: {
     color: "#FFF",
     fontSize: 24,
   },
   button: {
-    width:56,
-    height:56,
-    borderRadius:5,
-    backgroundColor:"#31CF67",
-    alignItems:"center",
-    justifyContent:"center",
+    width: 56,
+    height: 56,
+    borderRadius: 5,
+    backgroundColor: "#31CF67",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  form:{
-    width:"100%",
-    flexDirection:"row",
-   marginTop: 36,
-   marginBottom:42,
-  }
+  listEmptyText: {
+    color: "#FFF",
+    flexWrap:"wrap",
+    marginLeft:16,
+    marginRight:13,
+  },
+  form: {
+    width: "100%",
+    flexDirection: "row",
+    marginTop: 36,
+    marginBottom: 42,
+  },
 });
